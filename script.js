bc52db012c6310abf510af6f13714ca030f19fef
Alan Vardy <alanvardy@gmail.com>
2018-07-28 21:08:12 -0600

    Added html interface

--- script.js
+++ script.js
@@ -2,23 +2,47 @@ var computerScore = 0;
 var playerScore = 0;
 var roundsPlayed = 0;
 
-function computerPlay(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-    if (playerSelection == "rock" && computerSelection == "rock") {
+var choice = document.querySelectorAll(".choice");
+var reset = document.querySelector("#reset");
+var result = document.querySelector("#result");
+
+choice.forEach(function(e) {
+    e.addEventListener('click', function() {
+        playerSelection = this.textContent;
+        printScore(computerPlay(this.textContent));
+    })
+})
+
+reset.addEventListener('click', function() {
+    computerScore = 0;
+    playerScore = 0;
+    roundsPlayed = 0;
+    result.textContent = "";
+})
+
+function printScore(text) {
+    result.innerHTML = text + "<br> Player Score: " + playerScore +
+        "<br> Computer Score: " + computerScore +
+        "<br> Rounds played: " + roundsPlayed;
+}
+
+function computerPlay(playerSelection) {
+    var computerSelection = "Rock";
+    roundsPlayed++;
+    if (playerSelection == "Rock" && computerSelection == "Rock") {
+        return `It's a draw! The score is ${playerScore} to ${computerScore}`;
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Paper") {
         return "It's a draw!";
     }
-    else if (playerSelection == "paper" && computerSelection == "paper") {
+    else if (playerSelection == "Scissors" && computerSelection == "Scissors") {
         return "It's a draw!";
     }
-    else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        return "It's a draw!";
-    }
-    else if (playerSelection == "rock" && computerSelection == "scissors") {
+    else if (playerSelection == "Rock" && computerSelection == "Scissors") {
         playerScore++;
         return `You Win! ${playerSelection} beats ${computerSelection}`;
     }
-    else if (playerSelection == "paper" && computerSelection == "rock") {
+    else if (playerSelection == "Paper" && computerSelection == "Rock") {
         playerScore++;
         return `You Win! ${playerSelection} beats ${computerSelection}`;
     }
@@ -26,34 +50,16 @@ function computerPlay(playerSelection, computerSelection) {
         playerScore++;
         return `You Win! ${playerSelection} beats ${computerSelection}`;
     }
-    else if (playerSelection == "rock" && computerSelection == "paper") {
+    else if (playerSelection == "Rock" && computerSelection == "Paper") {
         computerScore++;
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
-    else if (playerSelection == "paper" && computerSelection == "scissors") {
+    else if (playerSelection == "Paper" && computerSelection == "Scissors") {
         computerScore++;
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
-    else if (playerSelection == "scissors" && computerSelection == "rock") {
+    else if (playerSelection == "Scissors" && computerSelection == "Rock") {
         computerScore++;
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
-}
-
-function playRound() {
-    var x = prompt("Enter player selection (rock, paper, or scissors)");
-    var y = prompt("Enter computer selection (rock, paper, or scissors)");
-    console.log(computerPlay(x, y));
-    console.log(`Current score is Player: ${playerScore} Computer ${computerScore}`)
-    roundsPlayed++;
-}
-
-while (roundsPlayed < 5) {
-    playRound();
-}
-if (playerScore > computerScore) {
-    console.log(`You won after ${roundsPlayed} rounds played. The score was ${playerScore} to ${computerScore}`);
-}
-else if (playerScore > computerScore) {
-    console.log(`You lost after ${roundsPlayed} rounds played. The score was ${playerScore} to ${computerScore}`);
 }
